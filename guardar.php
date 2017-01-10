42a42483281384c438794a04a3d3c44da3712492
rayavil <ramon_avil@hotmail.com>
2017-01-10 15:41:04 -0600

    Quitamos la validacion en php

--- guardar.php
+++ guardar.php
@@ -6,18 +6,9 @@ include('comm/datab.php');
 
 foreach ($_POST['valor'] as $item => $value){
 
-	if ($value=="") {
-		echo ' <script type="text/javascript">
-			var pregunta = '$item';
-           	alert("Por favor responda a la pregunta ".pregunta);
-  			history.back();
-			
-           
-       </script>';
-	}
+
      echo $item.": ".$value."<br>";
+
 }
-
-
 
 ?>
